3423f188b4caa82d4c78d5966f3f83af1800eedb
zeynepyildiz1 <azeynepyildiz1@gmail.com>
2021-06-03 12:34:14 +0300

    final

---
+++ src/app/app.module.ts
@@ -0,0 +1,44 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import {BrowserAnimationsModule} from "@angular/platform-browser/animations"
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { NaviComponent } from './components/navi/navi.component';
+import { ProductComponent } from './components/product/product.component';
+import { CategoryComponent } from './components/category/category.component';
+import {HttpClientModule, HTTP_INTERCEPTORS} from '@angular/common/http';
+import { VatAddedPipe } from './pipes/vat-added.pipe';
+import { FilterPipePipe } from './pipes/filter-pipe.pipe'
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { ToastrModule } from 'ngx-toastr';
+import { CartSummaryComponent } from './components/cart-summary/cart-summary.component';
+import { ProductAddComponent } from './components/product-add/product-add.component';
+import { LoginComponent } from './components/login/login.component';
+import { AuthInterceptor } from './Interceptors/auth.interceptor';
+@NgModule({
+  declarations: [
+    AppComponent,
+    NaviComponent,
+    ProductComponent,
+    CategoryComponent,
+    VatAddedPipe,
+    FilterPipePipe,
+    CartSummaryComponent,
+    ProductAddComponent,
+    LoginComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    HttpClientModule,
+    FormsModule,
+    ToastrModule.forRoot(),
+    BrowserAnimationsModule,
+    ReactiveFormsModule
+  ],
+  providers: [{
+    provide:HTTP_INTERCEPTORS, useClass:AuthInterceptor,multi:true
+  }],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
